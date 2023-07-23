9a125172ce69cfc08bdc8ec3d3225373411f609c
shalem43 <shalemministries11@gmail.com>
2023-07-23 20:05:29 +0530

    Updates

---
+++ precache-manifest.968eed865f7f63e4e4ce5192ff50b064.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "80fdf74ed121a4db8389",
+    "url": "/github.io/JJfoundation/static/css/main.78334250.chunk.css"
+  },
+  {
+    "revision": "80fdf74ed121a4db8389",
+    "url": "/github.io/JJfoundation/static/js/main.a0adb96d.chunk.js"
+  },
+  {
+    "revision": "2e094b4e817b77569595",
+    "url": "/github.io/JJfoundation/static/js/runtime~main.6a3058e8.js"
+  },
+  {
+    "revision": "fbdd3dedc55651bcadd2",
+    "url": "/github.io/JJfoundation/static/js/2.f65e3b21.chunk.js"
+  },
+  {
+    "revision": "490003ba4d80721aafcd05aeed4867af",
+    "url": "/github.io/JJfoundation/index.html"
+  }
+];
