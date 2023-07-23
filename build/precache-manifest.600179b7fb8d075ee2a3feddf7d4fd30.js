f82510ed61d7d73e5303bbf2cdadff7138a6c682
shalem43 <shalemministries11@gmail.com>
2023-07-23 20:28:54 +0530

    first issue

---
+++ build/precache-manifest.600179b7fb8d075ee2a3feddf7d4fd30.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "80fdf74ed121a4db8389",
+    "url": "/JJfoundation/static/css/main.78334250.chunk.css"
+  },
+  {
+    "revision": "80fdf74ed121a4db8389",
+    "url": "/JJfoundation/static/js/main.a0adb96d.chunk.js"
+  },
+  {
+    "revision": "ed2c231191e6d068bd0a",
+    "url": "/JJfoundation/static/js/runtime~main.dad39324.js"
+  },
+  {
+    "revision": "fbdd3dedc55651bcadd2",
+    "url": "/JJfoundation/static/js/2.f65e3b21.chunk.js"
+  },
+  {
+    "revision": "bae451974909c1069730402971e88ea5",
+    "url": "/JJfoundation/index.html"
+  }
+];
